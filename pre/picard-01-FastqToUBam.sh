07d83290ea015cbb6d7a308acea2af68824e1765
Taejoon Kwon <tkwon@unist.ac.kr>
2020-09-06 00:55:35 +0900

    revise the argument format.

--- pre/picard-01-FastqToUBam.sh
+++ pre/picard-01-FastqToUBam.sh
@@ -5,8 +5,8 @@ SAMPLE="mydata"
 FQ1=$SAMPLE"_R1.raw.fastq.gz"
 FQ2=$SAMPLE"_R2.raw.fastq.gz"
 
-OUT=$SAMPLE".ubam"
+UBAM=$SAMPLE".ubam"
 
 TMP_DIR="./tmp/"
 
-picard FastqToSam F1=$FQ1 F2=$FQ2 O=$OUT SM=$SAMPLE TMP_DIR=$TMP_DIR
+picard FastqToSam -F1 $FQ1 -F2 $FQ2 -O $UBAM -SM $SAMPLE -TMP_DIR $TMP_DIR
