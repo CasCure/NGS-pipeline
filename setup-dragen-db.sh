8648129f50ac5538413e26002d94e67d0d0b925c
Taejoon Kwon <tkwon@unist.ac.kr>
2020-12-19 12:16:12 +0900

    a script to setup DRAGEN hash db

--- setup-dragen-db.sh
+++ setup-dragen-db.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mkdir $HOME/db
-cd $HOME/db/
+DB_DIR="/ephemeral/db/"
+HASH_TGZ="hg38_ref.k_21.f_16.m_149.dragen_hash.tgz"
 
-aws s3 cp s3://cascure-active/db/hg38_ref.k_21.f_16.m_149.dragen_hash.tgz $HOME/db/
-aws s3 cp s3://cascure-active/db/hg38_ref.MSIsensor-pro.list.gz $HOME/db/
-tar cvzpf hg38_ref.k_21.f_16.m_149.dragen_hash.tgz
-gunzip hg38_ref.MSIsensor-pro.list.gz
+if [ ! -e $DB_DIR ]; then
+    mkdir $DB_DIR
+fi
+
+aws s3 cp s3://cascure-active/db/$HASH_TGZ $DB_DIR
+cd $DB_DIR
+tar xvzpf $HASH_TGZ
